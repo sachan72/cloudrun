f16bc5c6c4dab13fa4c3dbb4e1b38a4a09d5c3cb
sachan72 <61386070+sachan72@users.noreply.github.com>
2026-04-15 00:08:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from Cloud Run (Python)!"
+    return "Hello, World from Cloud Run (Python)! - with changes"
 
 if __name__ == "__main__":
     import os
